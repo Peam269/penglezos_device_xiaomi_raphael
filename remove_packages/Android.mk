b99ae855aa5c57316b28f4036a1a7b4e67e40441
Emil <emilwagenplast@gmail.com>
2023-03-03 00:37:12 +0100

    raphael: update list of removed packages at build time

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt DevicePolicyPrebuilt Eleven PixelWallpapers2022 SoundAmplifierPrebuilt
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps YouTube YoutubeMusicPrebuilt PrebuiltGmail Drive Maps
+LOCAL_OVERRIDES_PACKAGES := arcore AmbientSensePrebuilt AppDirectedSMSService Camera2 CarrierSetup ConnMO ConnMetrics DCMO DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService GCS GoogleTTS HelpRtcPrebuilt MaestroPrebuilt MyVerizonServices NovaBugreportWrapper OBDM_Permissions OemDmTrigger obdm_stub PixelLiveWallpaperPrebuilt PixelWallpapers2021 PixelWallpapers2022 PrebuiltGmail RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM talkback Tycho USCCDM VZWAPNLib VzwOmaTriger
+LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Drive Maps YouTube YoutubeMusicPrebuilt PrebuiltGmail
 LOCAL_OVERRIDES_PACKAGES := Phonograph yetCalc
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
