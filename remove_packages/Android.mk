221392feec878dee9530529115bef21ae035e0be
Emil <emilwagenplast@gmail.com>
2023-03-03 11:49:46 +0100

    raphael: update list of removed packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,9 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore AmbientSensePrebuilt AppDirectedSMSService Camera2 CarrierSetup ConnMO ConnMetrics DCMO DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService GCS GoogleTTS HelpRtcPrebuilt MaestroPrebuilt MyVerizonServices NovaBugreportWrapper OBDM_Permissions OemDmTrigger obdm_stub PixelLiveWallpaperPrebuilt PixelWallpapers2021 PixelWallpapers2022 PrebuiltGmail RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM talkback Tycho USCCDM VZWAPNLib VzwOmaTriger
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Drive Maps YouTube YoutubeMusicPrebuilt PrebuiltGmail
-LOCAL_OVERRIDES_PACKAGES := Phonograph yetCalc
+LOCAL_OVERRIDES_PACKAGES := arcore AmbientSensePrebuilt AppDirectedSMSService Camera2 CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics DCMO DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService Drive GCS GoogleTTS HelpRtcPrebuilt MaestroPrebuilt Maps MyVerizonServices NovaBugreportWrapper OBDM_Permissions OemDmTrigger obdm_stub PixelLiveWallpaperPrebuilt PixelWallpapers2021 PrebuiltGmail RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM talkback Tycho USCCDM VZWAPNLib VzwOmaTriger YouTube YouTubeMusicPrebuilt Phonograph yetCalc
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
